e19555049d4403655d45092e4194d3110ce97b10
ellan8888 <ellanaryaalqohari8888@gmail.com>
2026-09-19 19:47:01 +0700

    linear lagi

--- app/admin/stock/page.tsx
+++ app/admin/stock/page.tsx
@@ -372,7 +372,7 @@ export default function StockPage() {
           <button
             type="submit"
             disabled={saving || loadingKategori}
-            className="w-full bg-gradient-to-r from-violet-600 to-indigo-600 hover:from-violet-500 hover:to-indigo-500 disabled:opacity-50 text-white font-bold py-3 rounded-xl transition"
+            className="w-full bg-linear-to-r from-violet-600 to-indigo-600 hover:from-violet-500 hover:to-indigo-500 disabled:opacity-50 text-white font-bold py-3 rounded-xl transition"
           >
             {saving ? "Menyimpan..." : "➕ Tambah Stock"}
           </button>
